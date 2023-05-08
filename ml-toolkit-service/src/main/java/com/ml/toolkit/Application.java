f29c880ade70956a7b5f2939bd0b0af4e107f954
hxd <15136200812@163.com>
2023-05-08 20:37:47 +0800

    feat: add mybatis-plus 新增真实批量屋里插入测试demo

---
+++ ml-toolkit-service/src/main/java/com/ml/toolkit/Application.java
@@ -0,0 +1,25 @@
+package com.ml.toolkit;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author ml
+ * EnableEurekaClient 启动eurakeClient
+ * EnableOpenApi 启动openApi 生产HTML页面
+ */
+@SpringBootApplication
+@ComponentScan("com.ml.toolkit")
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+}
---
+++ ml-toolkit-service/src/main/java/com/ml/toolkit/mybatis/plus/demo/MybatisPlusController.java
@@ -0,0 +1,38 @@
+package com.ml.toolkit.mybatis.plus.demo;
+
+import com.ml.toolkit.common.collection.ListUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ml
+ * @date 2023年05月08日 20:35
+ */
+@RestController
+@RequestMapping("/mybatisPlus")
+public class MybatisPlusController {
+
+    @Resource
+    private DemoDao demoDao;
+
+    @RequestMapping("/test")
+    public void test() {
+        List<Demo> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            Demo demo = new Demo();
+            demo.setName("ml" + i);
+            demo.setAge(i);
+            demo.setGender(1);
+            list.add(demo);
+        }
+        // 自定义方法插入,执行分片
+        List<List<Demo>> partition = ListUtil.partition(list, 100);
+        for (List<Demo> demoList : partition) {
+            demoDao.insertBatchSomeColumn(demoList);
+        }
+    }
+}
