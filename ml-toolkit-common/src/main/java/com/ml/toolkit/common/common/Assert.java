1ef56a6f65c86d745a1b8d2eaa7ec22e3e3af33c
MuluoXue <muluolin@gmail.com>
2023-05-21 19:55:35 +0800

    feat: add Assert

---
+++ ml-toolkit-common/src/main/java/com/ml/toolkit/common/common/Assert.java
@@ -0,0 +1,21 @@
+package com.ml.toolkit.common.common;
+
+import com.ml.toolkit.common.util.ObjectUtil;
+
+import java.io.Serializable;
+
+public class Assert implements Serializable {
+
+    /**
+     * 为空断言
+     * @param errorMsg 错误提示语
+     * @param params 参数
+     */
+    public static void notEmpty(String errorMsg, Object ...params) {
+        for (Object param : params) {
+            if (ObjectUtil.isEmpty(param)) {
+                throw new RuntimeException(errorMsg);
+            }
+        }
+    }
+}
