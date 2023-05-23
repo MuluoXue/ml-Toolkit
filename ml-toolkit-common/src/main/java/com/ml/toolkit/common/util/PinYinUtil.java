832b672d03d5a78b04d5a53c9848c6b0b5516337
MuluoXue <muluolin@gmail.com>
2023-05-23 22:52:00 +0800

    perf: update PinYinUtil

--- ml-toolkit-common/src/main/java/com/ml/toolkit/common/util/PinYinUtil.java
+++ ml-toolkit-common/src/main/java/com/ml/toolkit/common/util/PinYinUtil.java
@@ -6,63 +6,83 @@ import java.io.Serializable;
 
 /**
  * 基于pinyin4j 进行封装
+ *
  * @author ml
  */
 public class PinYinUtil implements Serializable {
     private static final long serialVersionUID = 3375357938631197025L;
 
     /**
-     * 获取汉字首字母的方法。如： 张三 --> ZS
+     * 获取汉字首字母的方法。如： 张三 --> zs
      * 说明：暂时解决不了多音字的问题，只能使用取多音字的第一个音的方案
      * 非中文会自动过滤
-     * @param str 汉子字符串
-     * @return 大写汉子首字母; 如果都转换失败,那么返回null
+     *
+     * @param str 汉字字符串
+     * @return 大写汉字首字母; 如果都转换失败,那么返回null
      */
     public static String getHanZiInitials(CharSequence str) {
-        String result = null;
-        if(StringUtil.isNotEmpty(str)) {
+        if (StringUtil.isNotEmpty(str)) {
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < str.length(); i++) {
-                char charAt = str.charAt(i);
-                // 逐个汉字进行转换， 每个汉字返回值为一个String数组（因为有多音字）
-                String[] stringArray = PinyinHelper.toHanyuPinyinStringArray(charAt);
-                if(null != stringArray) {
-                    //遇见多音字取第一个
-                    sb.append(stringArray[0].charAt(0));
-                }
+                sb.append(getHanZiInitials(str.charAt(i)));
             }
-            if(sb.length() > 0) {
-                result = sb.toString().toUpperCase();
-            }
+            return sb.toString();
         }
-        return result;
+        return null;
     }
 
     /**
-     * 获取汉字拼音的方法。如： 张三 --> zhangsan
+     * 获取汉字首字母的方法
+     * 说明：暂时解决不了多音字的问题，只能使用取多音字的第一个音的方案
+     * 非中文会自动过滤
+     *
+     * @param charAt 汉字字符串
+     * @return 大写汉字首字母; 如果都转换失败,那么返回 ""
+     */
+    public static String getHanZiInitials(char charAt) {
+        // 逐个汉字进行转换， 每个汉字返回值为一个String数组（因为有多音字）
+        String[] stringArray = PinyinHelper.toHanyuPinyinStringArray(charAt);
+        if (null != stringArray) {
+            //遇见多音字取第一个
+            return stringArray[0].charAt(0) + "";
+        }
+        return "";
+    }
+
+    /**
+     * 获取汉字拼音的方法。如： 张 --> zhang
+     * 说明：暂时解决不了多音字的问题，只能使用取多音字的第一个音的方案
+     * 非中文会自动过滤
+     *
+     * @param charAt 汉字
+     * @return 汉字首字母; 如果都转换失败,那么返回 ""
+     */
+    public static String getHanZiPinYin(char charAt) {
+        // 逐个汉字进行转换， 每个汉字返回值为一个String数组（因为有多音字）
+        String[] stringArray = PinyinHelper.toHanyuPinyinStringArray(charAt);
+        if (null != stringArray) {
+            // 遇见多音字取第一个,把第几声这个数字给去掉
+            return stringArray[0].replaceAll("\\d", "");
+        }
+        return "";
+    }
+
+    /**
+     * 获取汉字拼音的方法。如： 张 --> zhang
      * 说明：暂时解决不了多音字的问题，只能使用取多音字的第一个音的方案
      *
-     * @param str 汉子字符串
+     * @param str 汉字字符串
      * @return 汉字拼音; 如果都转换失败,那么返回null
      */
     public static String getHanZiPinYin(CharSequence str) {
-        String result = null;
-        if(StringUtil.isNotEmpty(str)) {
+        if (StringUtil.isNotEmpty(str)) {
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < str.length(); i++) {
-                char charAt = str.charAt(i);
-                // 逐个汉字进行转换， 每个汉字返回值为一个String数组（因为有多音字）
-                String[] stringArray = PinyinHelper.toHanyuPinyinStringArray(charAt);
-                if(null != stringArray) {
-                    //
-                    // 遇见多音字取第一个,把第几声这个数字给去掉
-                    sb.append(stringArray[0].replaceAll("\\d", ""));
-                }
+                // 遇见多音字取第一个,把第几声这个数字给去掉
+                sb.append(getHanZiPinYin(str.charAt(i)));
             }
-            if(sb.length() > 0) {
-                result = sb.toString().toUpperCase();
-            }
+            return sb.toString();
         }
-        return result;
+        return null;
     }
 }
